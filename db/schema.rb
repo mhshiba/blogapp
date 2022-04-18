d150685c641d1472bb205f3285e11a30df2b40d3
Marcelo Shiba <marcelo.shiba@pipefy.com>
2022-04-18 10:48:58 -0300

    :sparkles: Run rails db:migrate for Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_18_134440) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_18_134745) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -22,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_18_134440) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
